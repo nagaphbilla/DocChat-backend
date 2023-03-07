3b6db9f0edba71ab080a57f737b9175b0f0d156a
Naga <nptbilla@gmail.com>
2023-03-07 19:17:00 +0530

    Added models

---
+++ models/userSchema.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+
+const userSchema = new mongoose.Schema(
+    {
+        name : {
+            type : String,
+            required : true,
+        },
+        email : {
+            type : String,
+            required : true,
+            unique : true
+        },
+        password : {
+            type : String,
+            required : true,
+            min : 8
+        },
+        rooms : {
+            type : [String],
+            default : []
+        }
+    }
+)
+
+module.exports = mongoose.model("User", userSchema)
